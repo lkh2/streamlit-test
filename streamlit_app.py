68a0ba058835848fcb0794f6deb66224c82c499f
Ka Ho Li <leekaho100@gmail.com>
2025-01-11 14:30:33 +0800

    Remove dnspython

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,38 @@ import streamlit as st
 from pymongo import MongoClient
 import pandas as pd
 from pandas import json_normalize
+from bson import ObjectId
 
 # Initialize connection.
 @st.cache_resource
 def init_connection():
     mongo_connection_string = (
-        f"mongodb+srv://{st.secrets['mongo']['username']}:"
-        f"{st.secrets['mongo']['password']}@{st.secrets['mongo']['host']}/"
+        f"mongodb+srv://{st.secrets['mongo']['username']}:" 
+        f"{st.secrets['mongo']['password']}@{st.secrets['mongo']['host']}/" 
         f"{st.secrets['mongo']['database']}?retryWrites=true&w=majority"
     )
     return MongoClient(mongo_connection_string)
 
 client = init_connection()
 
+def convert_mongo_doc(doc):
+    # Convert ObjectId to string
+    if '_id' in doc:
+        doc['_id'] = str(doc['_id'])
+    return doc
+
 # Pull data from the collection.
 @st.cache_data(ttl=600)
 def get_data():
     db = client[st.secrets["mongo"]["database"]]
     collection = db[st.secrets["mongo"]["collection"]]
-    items = collection.find().limit(200)  # Limit to the first 200 entries
-    items = list(items)  # Make hashable for st.cache_data
-    return items
+    items = collection.find().limit(200)
+    # Convert ObjectId to string for each document
+    processed_items = []
+    for item in items:
+        item['_id'] = str(item['_id'])
+        processed_items.append(item)
+    return processed_items
 
 items = get_data()
 
